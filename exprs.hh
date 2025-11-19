b7bdda5ec48b4febdfaef4d1f4c27a91e9f1ba25
hourin <phucjaki@gmail.com>
2025-11-19 22:32:15 +0700

    Added: ConstLiteral and Identifier

--- exprs.hh
+++ exprs.hh
@@ -1,12 +1,24 @@
 #pragma once
 
+#include <cstdint>
+
 #include <variant>
+#include <string>
 
 struct SourceInfo {
 };
 
+using ConstLiteral = std::variant<int64_t, double, std::string>;
+
+struct Identifier {
+        std::string name;
+};
+
 struct Expr {
         SourceInfo inf;
-        std::variant<std::monostate> v;
+        std::variant
+                < std::monostate
+                , ConstLiteral
+                > v;
 };
 
